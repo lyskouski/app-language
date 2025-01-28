d3ba28d23898e3a2348e4db4888a6242436e7a07
Viachaslau Lyskouski <deus@creativity.by>
2025-01-28 09:45:45 +0100

    [#1] [NF] Initial setup. Add routing

--- src/main.py
+++ src/main.py
@@ -6,10 +6,22 @@
 import kivy
 
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+
+class RootWidget(BoxLayout):
+    container = ObjectProperty(None)
 
 class MainApp(App):
-  kv_directory = 'template'
+    kv_directory = 'template'
 
+    def next_screen(self, screen):
+        filepath = self.kv_directory + '/' + screen + '.kv'
+        Builder.unload_file(filepath)
+        self.root.container.clear_widgets()
+        screen = Builder.load_file(filepath)
+        self.root.container.add_widget(screen)
 
 if __name__ == '__main__':
     MainApp().run()
