978eec698a54d64a55bde382fa379d910031364d
Viachaslau Lyskouski <deus@creativity.by>
2025-02-03 15:44:48 +0100

    [#1] [NF] Initial setup. Validate status

--- src/component/harmonica_widget.py
+++ src/component/harmonica_widget.py
@@ -5,6 +5,7 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.uix.image import Image
 
 class HarmonicaWidget(ScrollView):
     origin = BooleanProperty(False)
@@ -27,6 +28,7 @@ class HarmonicaWidget(ScrollView):
 
     def add_row(self, layout, origin, trans):
         row = BoxLayout(orientation='horizontal')
+        text_input = False
 
         if self.origin and self.secondary:
             row.add_widget(Label(text=origin))
@@ -34,10 +36,47 @@ class HarmonicaWidget(ScrollView):
 
         elif self.origin and not self.secondary:
             row.add_widget(Label(text=origin))
-            row.add_widget(TextInput(text=trans))
+            # row.add_widget(TextInput(text=trans))
+            text_input = TextInput(text="", multiline=False)
+            text_input.bind(on_text_validate=self.validate_trans)
+            row.add_widget(text_input)
 
         elif not self.origin and self.secondary:
             row.add_widget(Label(text=trans))
-            row.add_widget(TextInput(text=origin))
+            # row.add_widget(TextInput(text=origin))
+            text_input = TextInput(text="", multiline=False)
+            text_input.bind(on_text_validate=self.validate_origin)
+            row.add_widget(text_input)
+
+        if text_input:
+            success_icon = Image(source="assets/images/success.png", size_hint=(None, None), size=(30, 30), opacity=0)
+            error_icon = Image(source="assets/images/error.png", size_hint=(None, None), size=(30, 30), opacity=0)
+            row.add_widget(success_icon)
+            row.add_widget(error_icon)
+            text_input.success_icon = success_icon
+            text_input.error_icon = error_icon
 
         layout.add_widget(row)
+
+    def __get_pair(self, instance, is_origin):
+        key = instance.parent.children[3].text.strip()
+        app = App.get_running_app()
+        for origin, trans in app.data:
+            if origin == key or trans == key:
+                return origin if is_origin else trans
+            
+    def __validate(self, instance, is_origin):
+        text = instance.text.strip()
+        pair = self.__get_pair(instance, is_origin)
+        if (pair == text):
+            instance.success_icon.opacity = 1
+            instance.error_icon.opacity = 0
+        else:
+            instance.success_icon.opacity = 0
+            instance.error_icon.opacity = 1
+
+    def validate_origin(self, instance):
+        self.__validate(instance, True)
+
+    def validate_trans(self, instance):
+        self.__validate(instance, False)
