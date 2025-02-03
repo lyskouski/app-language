ee39b4fbf5bf05552906a0fcc85a1c5ea2a84683
Viachaslau Lyskouski <deus@creativity.by>
2025-02-03 09:59:12 +0100

    [#1] [NF] Initial setup. Add pages

---
+++ src/component/harmonica_widget.py
@@ -0,0 +1,6 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import BooleanProperty
+
+class HarmonicaWidget(BoxLayout):
+    origin = BooleanProperty(False)
+    secondary = BooleanProperty(False)
--- src/main.py
+++ src/main.py
@@ -7,14 +7,20 @@ import kivy
 
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, BooleanProperty, StringProperty
 from kivy.uix.boxlayout import BoxLayout
+from kivy.utils import platform
 
 class RootWidget(BoxLayout):
     container = ObjectProperty(None)
 
 class MainApp(App):
-    kv_directory = 'template'
+    kv_directory = StringProperty('template')
+    is_mobile = BooleanProperty(False)
+
+    def build(self):
+        if platform in ['android', 'ios']:
+            self.is_mobile = True
 
     def next_screen(self, screen):
         filepath = self.kv_directory + '/' + screen + '.kv'
