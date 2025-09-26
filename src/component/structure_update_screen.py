afe66d5b5f9300963480f32e6c6a9778aafca2a3
Viachaslau Lyskouski <deus@creativity.by>
2025-09-26 15:04:10 +0200

    [#20] [NF] Edit course structure. Save data

--- src/component/structure_update_screen.py
+++ src/component/structure_update_screen.py
@@ -1,4 +1,7 @@
+import json
+import os
 
+from kivy.app import App
 from kivy.properties import NumericProperty, StringProperty
 from kivy.uix.screenmanager import Screen
 
@@ -24,3 +27,43 @@ class StructureUpdateScreen(Screen):
             self.route_path = widget.route_path
             self.locale = widget.locale
             self.locale_to = widget.locale_to
+
+    def update_data(self):
+        app = App.get_running_app()  
+        if self.source != '':
+            os.makedirs(os.path.join(app.get_home_dir(), os.path.dirname(self.source)), exist_ok=True)
+        if self.store_path != '':
+            store_full_path = os.path.join(app.get_home_dir(), self.store_path)
+            os.makedirs(os.path.dirname(store_full_path), exist_ok=True)
+            if not os.path.exists(store_full_path):
+                with open(store_full_path, 'w', encoding='utf-8') as f:
+                    f.write('original ; translation ; audio file name with extension (.mp3)\n')
+        data = []
+        initial_path = app.find_resource(self.parent_source)
+        if initial_path is not None and os.path.exists(initial_path):
+            with open(initial_path, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+
+        insert_idx = None
+        new_data = [{
+            'text': self.text,
+            'source': self.source,
+            'logo': self.logo,
+            'store_path': self.store_path,
+            'route_path': self.route_path,
+            'locale': self.locale,
+            'locale_to': self.locale_to,
+        }]
+        for idx, item in enumerate(data):
+            if isinstance(item, dict) and item.get('text') == self.text_initial:
+                insert_idx = idx
+                break
+        if insert_idx is not None:
+            data = data[:insert_idx] + new_data + data[insert_idx+1:]
+        else:
+            data += new_data
+
+        source_path = os.path.join(app.get_home_dir(), self.parent_source)
+        os.makedirs(os.path.dirname(source_path), exist_ok=True)
+        with open(source_path, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
