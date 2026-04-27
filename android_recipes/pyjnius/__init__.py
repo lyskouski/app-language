a88f341a970b7417af127ea96c7746f5429e022d
Viachaslau Lyskouski <deus@creativity.by>
2026-04-27 17:02:44 +0200

    [#73] [CI] [Android] Build failure. Add recipes

---
+++ android_recipes/pyjnius/__init__.py
@@ -0,0 +1,24 @@
+from pythonforandroid.recipes.pyjnius import PyjniusRecipe as OriginalPyjniusRecipe
+from pythonforandroid.logger import shprint
+from pythonforandroid.util import current_directory
+import sh
+
+
+class PyjniusRecipe(OriginalPyjniusRecipe):
+    """
+    Custom PyJNIus recipe that forces traditional setup.py build
+    to avoid isolated environment issues with python -m build
+    """
+
+    # Force the recipe to NOT use call_hostpython_via_targetpython
+    # This makes it use the traditional build approach
+    call_hostpython_via_targetpython = False
+
+    def build_arch(self, arch):
+        """Override build to use traditional setup.py install"""
+        super().prebuild_arch(arch)
+        self.install_python_package(arch)
+        super().postbuild_arch(arch)
+
+
+recipe = PyjniusRecipe()
--- buildozer.spec
+++ buildozer.spec
@@ -338,7 +338,7 @@ p4a.commit = 9720dbd5489ebf0e8490a9fb930828f4426b8a20
 #p4a.source_dir =
 
 # (str) The directory in which python-for-android should look for your own build recipes (if any)
-#p4a.local_recipes = ./android_recipes/
+p4a.local_recipes = ./android_recipes/
 
 # (str) Filename to the hook for p4a
 #p4a.hook =
