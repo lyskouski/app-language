8e687726d04b2cf1839e081279ba5dbdb4b6e877
Viachaslau Lyskouski <deus@creativity.by>
2026-06-05 11:29:05 +0200

    [#79] [BF] Polish language. Add localization

--- src/l18n/labels.py
+++ src/l18n/labels.py
@@ -1,11 +1,13 @@
 from l18n.labels_be import labels_be
 from l18n.labels_en import labels_en
+from l18n.labels_pl import labels_pl
 from l18n.labels_ru import labels_ru
 from l18n.labels_uk import labels_uk
 
 labels = {
     "BE": labels_be,
     "EN": labels_en,
+    "PL": labels_pl,
     "RU": labels_ru,
     "UK": labels_uk
 }
